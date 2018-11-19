5981659e74d4a32f531c3e6c1b496ecdacbcf5e9
del-naz <delnaz@mindstronghealth.com>
2018-11-18 19:14:03 -0800

    init7

--- js/vajeh.js
+++ js/vajeh.js
@@ -2,5 +2,12 @@ var options = {
   valueNames: [ 'title', 'description' ],
   item: '<li><h3 class="title"></h3><p class="description"></p></li>'
   };
+  
+  var vajehList;
 
-var vajehList = new List('vajeh-list', options, behList);
+function startVajeh() {
+  var vajehList = new List('vajeh-list', options, behList);
+}
+function searchList(str) {
+  vajehList.search(str, ['title'])
+}
