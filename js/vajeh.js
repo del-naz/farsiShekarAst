f1a69b5e5644981d31d95fcdf0e93fbd2c7260f5
del-naz <delnaz@mindstronghealth.com>
2018-11-21 12:43:48 -0800

    init9

--- js/vajeh.js
+++ js/vajeh.js
@@ -1,9 +1,10 @@
 var options = {
   valueNames: [ 'title', 'description' ],
-  item: '<li><table>' +
-        '<tr><td><h3 class="title"></td><td><h4>related</h4></td><td><h4>details</h4></td></tr>' +
-        '<tr><td><p class="description"></p></td><td><p class="related"></p></td><td><p class="details"></p></td></tr>' +
-        '</table></li>'
+  item: '<li>' +
+        '<div><h3 class="title"></h3><p class="description"></p><div> ' +
+        '<div><h4>واژه های مربوط</h4><p class="related"></p></div>' +
+        '<div><h4>توضیح/ مثال</h4><p class="details"></p></div>' +
+        '</li>'
   };
 
 var vajehList;
