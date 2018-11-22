2106d22fc84ca552170122e60a5bde5b810f18c9
del-naz <delnaz@mindstronghealth.com>
2018-11-22 11:35:43 -0800

    Updates

---
+++ precache-manifest.54b063d9a2611ed2848c5c84f67ea931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/farsiShekarAst/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00d679e4a6f64db147b8",
+    "url": "/farsiShekarAst/static/js/runtime~main.00d679e4.js"
+  },
+  {
+    "revision": "3a91d6e73e7b76f1d342",
+    "url": "/farsiShekarAst/static/js/main.3a91d6e7.chunk.js"
+  },
+  {
+    "revision": "6b0f44a2f42e66a62dd8",
+    "url": "/farsiShekarAst/static/js/1.6b0f44a2.chunk.js"
+  },
+  {
+    "revision": "3a91d6e73e7b76f1d342",
+    "url": "/farsiShekarAst/static/css/main.67b40ef4.chunk.css"
+  },
+  {
+    "revision": "28e508bcc9f647c422a803af8b88b4ba",
+    "url": "/farsiShekarAst/index.html"
+  }
+];
--- src/AlphaList.js
+++
@@ -1,7 +0,0 @@
-const AlphaList = {
-  alef: require('./data/alef.csv'),
-  beh: require('./data/beh.csv'),
-  feh: require('./data/feh.csv')
-}
-
-export default AlphaList
